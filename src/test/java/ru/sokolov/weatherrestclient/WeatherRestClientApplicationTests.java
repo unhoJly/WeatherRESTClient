d410d4288309039da9b93e0ae31ae9465a5cc301
UnhoJly <sokolov_it@mail.ru>
2023-07-20 10:15:58 +0300

    Initial commit

---
+++ src/test/java/ru/sokolov/weatherrestclient/WeatherRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sokolov.weatherrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
